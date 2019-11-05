79ee4a0f47016f9046a8b102361303806e5c53d5
tatsuki1112@gmail.com <tatsuki1112@gmail.com>
2019-11-06 00:11:36 +0900

    feat modalテスト

--- src/resources/views/inspection.blade.php
+++ src/resources/views/inspection.blade.php
@@ -11,18 +11,37 @@
             <img src="{{ asset('/image/robotsBG.png')}}" class='img-fluid mx-auto d-block'>
             <br>
             <div class="coutainer-fluid">
-            <form action="{{url('/inspect')}}" method="POST">
-                @csrf
+         
                     <br>
                     <div class="form-group">
                             <input type="number" class="form-control form-control-lg" id="trial" placeholder="回数を入力">
                     </div>
                     
                     <br>
-                    <button type="submit" class="btn3">
+                    <button type="button" class="btn3" data-toggle="modal" data-target="#modal1">
                         <span class='else'>じゃんけん!!!!</span>
                     </button>
-                </form> 
+                    <div class="modal fade" tabindex="-1" role="dialog" id="modal1" aria-hidden="true">
+                        <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                   
+                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                    </button>
+                                </div>
+                                    <div class="modal-body">
+                                    
+                                    <img src="{{ asset('/image/kairo2.png')}}" class='img-fluid mx-auto d-block'>
+                                    <br>
+                                    <br>
+                                    <br>
+                                    <br>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
             </div>
        </div>
        <div class='col-sm-1 col-md-3'></div>
