1ac38c59eaccc9e1f302c9ee222ede75ba8c1091
tatsuki1112@gmail.com <tatsuki1112@gmail.com>
2019-12-07 00:51:12 +0900

    JSを動かすことができるように、laravel mix,npmなどを使った

--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -12,15 +12,17 @@
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('/css/style.css') }}">
 </head>
-<body id="app-layout">
+<body id="app-layout" onLoad="testJS()">
 
 
 
 @yield('content')
 
 <!-- JavaScripts -->
+<script src="{{asset('/js/jangkeng.js')}}"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+
 </body>
 
 </html>
--- src/webpack.mix.js
+++ src/webpack.mix.js
@@ -12,4 +12,6 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
+    .scripts('resources/js/jangkeng.js', 'public/js/jangkeng.js')
     .sass('resources/sass/app.scss', 'public/css');
+    
